5fde52eca0cc8cac4b119b2344e4ac26c0e0c0fe
Amir Mardanov <mardanovamir2004@gmail.com>
2026-06-08 11:21:14 +0300

    текст фикс

--- handlers/duty.py
+++ handlers/duty.py
@@ -162,7 +162,7 @@ async def _render_feedback(
         )
 
     comment = (
-        f"\n\n{emoji.EMOJI_RED_2} <i>{question.comment}</i>"
+        f"\n\n 💬 <i>{question.comment}</i>"
         if question.comment else ""
     )
     body = (
